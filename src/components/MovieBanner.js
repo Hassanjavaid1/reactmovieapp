173adc8ca72ab08a3ce24ac7951ab95635a9e3cd
Hassanjavaid <hassanjavaid1214@gmail.com.com>
2025-01-28 00:48:32 +0500

    updated hero section

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import Gallery from "react-image-gallery";
-import './Demo.css'
+import "./Demo.css";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { HomeContext } from '../Api/HomeApi';
+import { FaRegCirclePlay } from "react-icons/fa6";
+
+import { HomeContext } from "../Api/HomeApi";
+import { Link } from "react-router-dom";
 
 function MovieBanner() {
-
-  const { heroBanners, backdropURL } = useContext(HomeContext)
+  const { heroBanners, backdropURL } = useContext(HomeContext);
 
   const options = {
     slideInterval: 4000,
@@ -14,38 +16,48 @@ function MovieBanner() {
     lazyLoad: true,
     showFullscreenButton: false,
     showPlayButton: false,
-  }
+  };
 
-  const slideGallery = heroBanners.map(({ backdrop_path, original_title, title, overview }) => ({
-    original: backdropURL + backdrop_path,
-    thumbnail: backdropURL + backdrop_path,
-    title: original_title || title,
-    description: overview || 'No Overview Found',
-  }))
+  const slideGallery = heroBanners.map(
+    ({ backdrop_path, original_title, title, overview, id }) => ({
+      id: id,
+      original: backdropURL + backdrop_path,
+      thumbnail: backdropURL + backdrop_path,
+      title: original_title || title,
+      description: overview || "No Overview Found",
+    })
+  );
 
   const gallerySlide = (item) => {
-
     return (
       <>
         <div className="custom-slide">
-          <img src={item.original} className='image-gallery-image' alt={item.title} />
+          <img
+          src={item.original}
+            className="image-gallery-image"
+            alt={item.title}
+          />
           <div className="slide-content">
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
+              <h1 className="slide-title">{item.title}</h1>
+              <p className="slide-desc">{item.description}</p>
+      <Link to={`/MovieDetail/${item.id}`}>
+            <button id="btn">
+              <FaRegCirclePlay id="buttonPlay" />
+              Watch Now
+            </button>
+    
+      </Link>
           </div>
-        </div>
+          </div>
       </>
-
-    )
-
-  }
-
+    );
+  };
 
   return (
     <>
       <Gallery items={slideGallery} renderItem={gallerySlide} {...options} />
     </>
-  )
+  );
 }
 
-export default MovieBanner
+export default MovieBanner;
